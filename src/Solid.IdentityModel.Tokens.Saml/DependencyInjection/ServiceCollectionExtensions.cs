3f7dd88e263473c43e2bd8e008b12e27297c9926
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2020-11-16 13:24:29 +0000

    Update ServiceCollectionExtensions.cs

--- src/Solid.IdentityModel.Tokens.Saml/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Solid.IdentityModel.Tokens.Saml/DependencyInjection/ServiceCollectionExtensions.cs
@@ -10,10 +10,20 @@ namespace Solid.IdentityModel.Tokens.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddSaml2Encryption(this IServiceCollection services)
+        public static IServiceCollection AddSaml2EncryptedSecurityTokenHandler(this IServiceCollection services)
+            => services.AddSaml2EncryptedSecurityTokenHandler<Saml2EncryptedSecurityTokenHandler>();
+
+        public static IServiceCollection AddSaml2EncryptedSecurityTokenHandler<THandler>(this IServiceCollection services)
+            where THandler : Saml2EncryptedSecurityTokenHandler
         {
-            services.TryAddTransient<Saml2SecurityTokenHandler, Saml2EncryptedSecurityTokenHandler>();
-            services.TryAddTransient<Saml2Serializer, ExtendedSaml2Serializer>();
+            services.TryAddTransient<Saml2SecurityTokenHandler, THandler>();
+            return services;
+        }
+
+        public static IServiceCollection AddExtendedSaml2Serializer<TSerializer>(this IServiceCollection services)
+            where TSerializer : ExtendedSaml2Serializer
+        {
+            services.TryAddTransient<Saml2Serializer, TSerializer>();
             return services;
         }
     }
